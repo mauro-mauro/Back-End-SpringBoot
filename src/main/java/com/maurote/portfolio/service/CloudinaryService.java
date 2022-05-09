743096a98d88b71b5366891bc4d650cfd93482c7
mauro-mauro <para_mauro@hotmail.com>
2022-05-09 09:36:08 -0300

    V-1.1

---
+++ src/main/java/com/maurote/portfolio/service/CloudinaryService.java
@@ -0,0 +1,58 @@
+package com.maurote.portfolio.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    Cloudinary cloudinary;
+
+    private Map<String, String> valuesMap = new HashMap<>();
+
+    public CloudinaryService(
+            @Value("${cloudinary.cloud-name}") String cloudName,
+            @Value("${cloudinary.api-key}") String apiKey,
+            @Value("${cloudinary.api-secret}") String apiSecret) {
+        System.out.println("cloud_name: " + cloudName);
+        System.out.println("apiKey: " + apiKey);
+        System.out.println("apiSecret: " + apiSecret);
+
+        valuesMap.put("cloud_name", cloudName);
+        valuesMap.put("api_key", apiKey);
+        valuesMap.put("api_secret", apiSecret);
+
+        cloudinary = new Cloudinary(valuesMap);
+    }
+
+    public Map upload(MultipartFile multipartFile) throws IOException {
+        File file = convert(multipartFile);
+        Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        file.delete();
+        return result;
+    }
+
+    public Map delete(String id) throws IOException {
+        Map result = cloudinary.uploader().destroy(id, ObjectUtils.emptyMap());
+        return result;
+    }
+
+    private File convert(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        FileOutputStream fo = new FileOutputStream(file);
+        fo.write(multipartFile.getBytes());
+        fo.close();
+        return file;
+    }
+
+}
